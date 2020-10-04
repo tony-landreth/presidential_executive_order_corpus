1b845e7da4a1a0f33f59de98f81317d8c84a3095
Anthony Landreth <anthony.w.landreth@gmail.com>
2020-10-04 19:37:51 -0400

    every executive order: 1995 - Oct 4, 2020

--- scrape_presidential_actions.rb
+++ scrape_presidential_actions.rb
@@ -4,14 +4,12 @@ require 'csv'
 csv_path = 'documents_of_type_presidential_document_and_of_presidential_document_type_executive_order.csv'
 table = CSV.parse(File.read(csv_path), headers: true)
 
-i = 0
-
 table.each do |r|
   url = r["html_url"]
+  eo_number = r["executive_order_number"]
   doc = Nokogiri::HTML(URI.open(url))
+  puts "URL: #{url}   EO NUMBER: #{eo_number}"
   target_html = doc.css("#fulltext_content_area").to_html
-  filename = doc.css(".eo").first.text.downcase.gsub(" ", "_") + ".html"
+  filename = eo_number + ".html"
   File.write(filename, target_html)
-  i += 1
-  break if i > 10
 end
